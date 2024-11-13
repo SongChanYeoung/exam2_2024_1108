54a2a5571698a888d4bd5a4708a58224bf75319a
SongChanYeong <scy283@naver.com>
2024-11-13 09:55:33 +0900

    JButton

--- src/main/java/TestSw.java
+++ src/main/java/TestSw.java
@@ -10,9 +10,20 @@ public class TestSw extends JFrame{
         setSize(500,300); // 프레임 크기 설정
         setLocationRelativeTo(null); // 화면 중앙에서 시작
 
+        // ContentPane
         Container cp = getContentPane(); // 프레임에서 컨텐트 팬 받아오기
+        // Layout
         cp.setLayout(null);
  //       cp.setLayout(new FlowLayout()); // 왼쪽에서 오른쪽으로 정렬
+        //JLabel
+        JLabel jLabel = new JLabel("Label Test");
+        jLabel.setBounds(100, 100, 100, 10);
+        cp.add(jLabel);
+
+        //JButton
+        JButton jButton = new JButton("Button Test");
+        jButton.setBounds(200, 200, 100 ,20);
+        cp.add(jButton);
 
         setVisible(true); // 화면에 프레임 출력
 
